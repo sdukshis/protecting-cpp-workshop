4a837a657a5fd4816509c9286cd7a57f71e4c4eb
Pavel Filonov <Pavel.Filonov@kaspersky.com>
2021-04-18 14:46:09 +0300

    smashing vptr example

---
+++ src/examples/04_stack_buff_overflow/03_smashing_vptr/vuln.cpp
@@ -0,0 +1,25 @@
+#include <string.h>
+#include <stdio.h>
+
+class Base {
+public:
+    virtual ~Base() {}
+    virtual void foo() = 0;
+};
+
+class Derived: public Base {
+public:
+    void foo() override {
+        printf("Derived foo\n");
+    }
+};
+
+
+int main(int argc, char* argv[]) {
+    char buff[64];
+    Base *base = new Derived{};
+    strcpy(buff, argv[1]);
+    base->foo();
+    delete base;
+    return 0;
+}
